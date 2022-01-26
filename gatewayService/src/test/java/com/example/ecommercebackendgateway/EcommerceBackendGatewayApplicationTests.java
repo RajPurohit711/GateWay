cf9642a87dd77daef1b012e7a00562cba689e3a1
alpeshtrigunayat <trigunayatalpesh@gmail.com>
2022-01-26 15:36:54 +0530

    initial commit

---
+++ gatewayService/src/test/java/com/example/ecommercebackendgateway/EcommerceBackendGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebackendgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
